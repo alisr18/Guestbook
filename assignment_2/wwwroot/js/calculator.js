bd6481c88c52cb47c4e16f2685178c7f4f176453
Ali Shahab Rezaii <alisr18@uia.no>
2021-10-11 05:37:27 +0200

    Delivered assignment 2

---
+++ assignment_2/wwwroot/js/calculator.js
@@ -0,0 +1,35 @@
+﻿let resultat = document.getElementById("display");
+
+let calculate=(number)=>{
+    resultat.value += number;
+}
+
+function Reset(){
+    resultat.value = ""
+}
+
+function clr(){
+    resultat.value = resultat.value.slice(0,-1)
+}
+
+let Result=()=>{
+    try {
+        
+        resultat.value = eval(resultat.value)
+        if (resultat.value === "undefined"){
+            resultat.value = "noice"
+        }
+    }
+    catch(err){
+        if("+-" in resultat.value){
+            resultat.value -= "+"
+            resultat.value = eval(resultat.value)
+        }
+        else if ("-+" in resultat.value){
+            resultat.value -= "-"
+            resultat.value = eval(resultat.value)
+        }
+    }
+    console.log(resultat.value)
+}
+
